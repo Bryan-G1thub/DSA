b1502aae950aae1463d1c6a4a104fea19c29e170
Bryan Ortiz <bryanortiz.inbox@gmail.com>
2024-08-25 11:58:10 -0700

    Created repo, with two algorithm classes; for viewing purposes.

---
+++ src/EvenNumPrinterV1.java
@@ -0,0 +1,20 @@
+public class EvenNumPrinterV1 {
+
+    public static int printEvenNums(int num) {
+        int steps = 0;
+        while (num <= 10) {
+            if (num % 2 == 0) {
+                System.out.println(num);
+            }
+            num++;
+            steps++;
+        }
+        System.out.println("Steps: " + steps);
+        return num;
+    }
+
+    public static void main(String[] args) {
+        int num = 2; 
+        printEvenNums(num);
+    }
+}
