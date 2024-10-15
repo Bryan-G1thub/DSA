d37317e09a4f56843d65851a572eaec4c62250e1
Bryan Ortiz <bryanortiz.inbox@gmail.com>
2024-10-14 20:53:48 -0700

    this gootta be it

--- src/longestCommonPrefix.java
+++ src/longestCommonPrefix.java
@@ -1,23 +1,30 @@
-class longestCommonPrefix{
+public class longestCommonPrefix{
     public static void main(String[] args) {
-        String[] strs = {"flower", "flow", "flight"};
-        System.out.println(Solution(strs));;
+        String[] strs = {"flower", "flow"};
+        System.out.println(Solution(strs));
     }
     public static String Solution(String[] strs){
-        
-        StringBuilder prefix = new StringBuilder();
-        for(int i = 0; i <= strs.length-1; i++){
-                for(int j = 0; i<strs[i].length()-1;i++){
-                  if(j != strs[i].length()-1 && strs[i].charAt(j) == strs[i+1].charAt(j)){
-                        prefix.append(strs[i].charAt(j));
+        StringBuilder sb = new StringBuilder();
+        char c;
+        for(int i  = 0; i<= strs[0].length()-1; i++){//loop flower
+            sb.append(strs[0].charAt(i));
+            for(int j = 1; j<= strs.length-1; j++){//loop strs[]
+                    
+                c = (strs[0].charAt(i));
+                    //if i > length of string j, break
+                if(i>strs[j].length()-1){
+                        sb.deleteCharAt(i);
+                        String answer = sb.toString();
+                        return answer;
                     }
-                    else{
-                        break;
+
+                    //if character at strs[j] index i, is not == c
+                    else if(strs[j].charAt(i) != c){
+                        sb.deleteCharAt(i);
+                        String answer = sb.toString();
+                        return answer;
                     }
                 }
-
+            }
         }
-        String answer = prefix.toString(); 
-        return answer;
-    }
 }
