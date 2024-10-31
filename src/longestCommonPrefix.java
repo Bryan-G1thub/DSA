143d220668f0260ac5d2083f94f8c2f170333730
Bryan Ortiz <bryanortiz.inbox@gmail.com>
2024-10-30 20:24:04 -0700

    Worked on longestCommonPrefix

--- src/longestCommonPrefix.java
+++ src/longestCommonPrefix.java
@@ -1,30 +1,15 @@
+import java.util.Arrays;
+
 public class longestCommonPrefix{
     public static void main(String[] args) {
         String[] strs = {"flower", "flow"};
-        System.out.println(Solution(strs));
-    }
-    public static String Solution(String[] strs){
-        StringBuilder sb = new StringBuilder();
-        char c;
-        for(int i  = 0; i<= strs[0].length()-1; i++){//loop flower
-            sb.append(strs[0].charAt(i));
-            for(int j = 1; j<= strs.length-1; j++){//loop strs[]
-                    
-                c = (strs[0].charAt(i));
-                    //if i > length of string j, break
-                if(i>strs[j].length()-1){
-                        sb.deleteCharAt(i);
-                        String answer = sb.toString();
-                        return answer;
-                    }
-
-                    //if character at strs[j] index i, is not == c
-                    else if(strs[j].charAt(i) != c){
-                        sb.deleteCharAt(i);
-                        String answer = sb.toString();
-                        return answer;
-                    }
-                }
-            }
+        Arrays.sort(strs);
+        for(String word : strs){
+            System.out.println(word);
         }
-}
+    }
+    /*public static String Solution(String[] strs){
+        Arrays.sort(strs);
+        System.out.println(strs);
+        */
+    }
