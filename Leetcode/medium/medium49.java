736d5ee61a839879ba45303f48434ee6acd85618
Bryan Ortiz <bryanortiz.inbox@gmail.com>
2025-02-07 14:55:40 -0800

    Added boilerplate

---
+++ Leetcode/medium/medium49.java
@@ -0,0 +1,11 @@
+package medium;
+import java.util.*;
+public class medium49 {
+    public static void main(String[] args) {
+        String[] strs = {"act","pots","tops ","cat","stop","hat"};
+        groupAnagrams(strs);
+    }   
+    public static List<List<String>> groupAnagrams(String[] strs){
+        //Gotta group all anagrams into sublists, any order
+    }
+}
