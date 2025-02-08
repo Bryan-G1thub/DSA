653e3048e76b2f79ae2688f6d9ab05d2e44219e0
Bryan Ortiz <131089148+Bryan-G1thub@users.noreply.github.com>
2025-02-08 16:04:36 -0500

    Not done

--- Leetcode/medium/medium49.java
+++ Leetcode/medium/medium49.java
@@ -6,6 +6,21 @@ public class medium49 {
         groupAnagrams(strs);
     }   
     public static List<List<String>> groupAnagrams(String[] strs){
-        //Gotta group all anagrams into sublists, any order
+        //create the return List<List<String>>
+        List<List<String>> groupedAnagrams = new ArrayList<>();
+        
+        //loop the given strs
+        for(int i = 0; i <= strs.length; i++){
+
+            //create a char[] of our current string
+            char[] chars = strs[i].toCharArray();
+
+            //sort the char[]
+            Arrays.sort(chars);
+
+            //
+
+    return null;
+        }
     }
 }
