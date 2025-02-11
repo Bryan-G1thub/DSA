d1f4cd08afc4345b11d8d2339b9786c28b79feed
Bryan Ortiz <bryanortiz.inbox@gmail.com>
2025-02-10 23:21:21 -0800

    Psuedo in progress

--- Leetcode/medium/medium49.java
+++ Leetcode/medium/medium49.java
@@ -6,21 +6,33 @@ public class medium49 {
         groupAnagrams(strs);
     }   
     public static List<List<String>> groupAnagrams(String[] strs){
-        //create the return List<List<String>>
+
+        HashMap <String, Boolean> map = new HashMap<>();
+
+        //1. create the return List<List<String>>
         List<List<String>> groupedAnagrams = new ArrayList<>();
         
-        //loop the given strs
-        for(int i = 0; i <= strs.length; i++){
+        //2. loop the given strs
+        for(int i = 0; i < strs.length; i++){
 
-            //create a char[] of our current string
+            //3. strs[i] -> char[]
             char[] chars = strs[i].toCharArray();
 
-            //sort the char[]
+            //4. sort it
             Arrays.sort(chars);
 
-            //
+            //5. convert back into string
+            String anagramCheck = new String(chars);
 
-    return null;
+            //6. check if the sort of this string has already been seen
+            if(!map.containsKey(strs[i])){
+            //NO: put the sorted string into map
+            map.put(anagramCheck, true); 
+            groupedAnagrams.get(i).add(strs[i]);
+            }
+            
         }
+                System.out.println(groupedAnagrams);
+                return groupedAnagrams;
     }
 }
