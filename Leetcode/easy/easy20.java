b7b1e1598209ef1c869a8e03e7820d5959e42596
Bryan Ortiz <bryanortiz.inbox@gmail.com>
2025-06-05 14:32:57 -0700

    easy20 progress

---
+++ Leetcode/easy/easy20.java
@@ -0,0 +1,20 @@
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.*/
+
+package easy;
+
+class easy20 {
+    public static void main(String[] args) {
+        String s = "()";
+    }
+    public static boolean validPaenthesisCheck(String s){
+        //map openers to closers
+        return false;
+    }
+}
