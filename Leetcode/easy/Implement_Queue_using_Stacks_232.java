254a0afed0cc0868d9351c3b79be63db27fd99a5
Bryan Ortiz <bryanortiz.inbox@gmail.com>
2025-06-25 10:37:25 -0700

    Completed with 0ms runtime, beats 100.00%

--- Leetcode/easy/Implement_Queue_using_Stacks_232.java
+++ Leetcode/easy/Implement_Queue_using_Stacks_232.java
@@ -7,25 +7,34 @@ public class Implement_Queue_using_Stacks_232 {
 
     }
     public static class MyQueue{
-        private Stack<Integer> STACK;
-        private Stack<Integer> REVERESED_STACK;
+        private Stack<Integer> STACK1; //enque
+        private Stack<Integer> STACK2; //dequue
 
         public MyQueue(){
-            STACK = new Stack<>();
-            REVERESED_STACK = new Stack<>();
+            STACK1 = new Stack<>();
+            STACK2  = new Stack<>();
         }
         public void push(int x){
-
-            TOP.push(x);
-
+            STACK1.push(x);
+        }
+        public int pop(){
+            if(STACK2.isEmpty()){
+                while(!STACK1.isEmpty()){
+                    STACK2.push(STACK1.pop());
+                }
+            }
+                return STACK2.pop();    
         }
         public int peek(){
-
-        return -1;
+           if(STACK1.isEmpty()){
+            while(!STACK1.isEmpty()){
+                    STACK2.push(STACK1.pop());
+               }
+            }
+            return STACK2.peek();
         }
         public boolean empty(){
-
-        return false;
+            return STACK1.isEmpty() && STACK2.isEmpty();
         }
     }
 }
