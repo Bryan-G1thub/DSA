dcd3e7b8a84dc8b241eb59a27f8325b15077d5f1
Bryan Ortiz <bryanortiz.inbox@gmail.com>
2025-06-27 13:14:44 -0700

    Created file and started boilerplate

---
+++ Leetcode/easy/Number_of_Students_Unable_to_Eat_Lunch_1700.java
@@ -0,0 +1,17 @@
+package easy;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Number_of_Students_Unable_to_Eat_Lunch_1700 {
+    public static void main(String[] args) {
+        int[] students;
+        int[] sandwiches;
+    }
+    public static int countStudents(){
+         Stack<Integer> stack = new Stack<>();
+         Queue<Integer> studentLine = new LinkedList<>();
+
+    }
+}
