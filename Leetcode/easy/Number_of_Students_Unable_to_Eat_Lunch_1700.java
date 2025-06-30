8be4f7d3e988330526b79b9cbb8e2a69a54938d4
Bryan Ortiz <bryanortiz.inbox@gmail.com>
2025-06-30 13:24:01 -0700

    Completed logic for populating Queue and Stack while removing matches

--- Leetcode/easy/Number_of_Students_Unable_to_Eat_Lunch_1700.java
+++ Leetcode/easy/Number_of_Students_Unable_to_Eat_Lunch_1700.java
@@ -4,14 +4,32 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
+import org.xml.sax.SAXNotRecognizedException;
+
 public class Number_of_Students_Unable_to_Eat_Lunch_1700 {
     public static void main(String[] args) {
-        int[] students;
-        int[] sandwiches;
+        int[] students = {1,1,1,0,0,1};
+        int[] sandwiches = {1,0,0,0,1,1};
+        countStudentsOPTIMIZED(students, sandwiches);
     }
-    public static int countStudents(){
-         Stack<Integer> stack = new Stack<>();
-         Queue<Integer> studentLine = new LinkedList<>();
-
+    public static int countStudentsOPTIMIZED(int[] students, int[] sandwiches){
+        Stack<Integer> SAMMICH_STACK = new Stack<>();
+        Queue<Integer> STUDENT_QUEUE = new LinkedList<>();
+        for(int i = 0; i<students.length; i++){
+            if(students[i]!=sandwiches[i]){
+                STUDENT_QUEUE.add(students[i]);
+                SAMMICH_STACK.add(sandwiches[sandwiches.length-1-i]);
+            }
+        }
+        int rotations = 0;
+        while (!STUDENT_QUEUE.isEmpty() && !SAMMICH_STACK.isEmpty()){
+            if (STUDENT_QUEUE.peek() == SAMMICH_STACK.peek()) {
+                 STUDENT_QUEUE.poll();
+                 SAMMICH_STACK.pop();
+                 rotations = 0;
+            }
+            else if()
+        return STUDENT_QUEUE.size();
+        }
     }
 }
