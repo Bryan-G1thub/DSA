a1d5997cb15ae08ded1064820d9133db31fafd15
Bryan Ortiz <bryanortiz.inbox@gmail.com>
2025-07-07 14:45:24 -0700

    Created file and wrote boilerplate for 509, easy recursion problem

---
+++ Leetcode/easy/Fibonacci_Numbers_509.java
@@ -0,0 +1,12 @@
+package easy;
+
+public class Fibonacci_Numbers_509 {
+    public static void main(String args[]){
+        int n = 3;
+    }
+    public static int fib(int n){
+        
+
+        fib(n);
+    }
+}
