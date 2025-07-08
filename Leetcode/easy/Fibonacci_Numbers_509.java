8d3361bf354061d9aebaf020228db906e5b32c7b
Bryan Ortiz <bryanortiz.inbox@gmail.com>
2025-07-08 11:42:36 -0700

    Completed, 9ms runtime, beats 23.32%

--- Leetcode/easy/Fibonacci_Numbers_509.java
+++ Leetcode/easy/Fibonacci_Numbers_509.java
@@ -3,10 +3,11 @@ package easy;
 public class Fibonacci_Numbers_509 {
     public static void main(String args[]){
         int n = 3;
+        System.out.println(fib(n));
     }
     public static int fib(int n){
-        
-
-        fib(n);
+        if(n==1) return 1;
+        if(n==0) return 0;
+        return fib(n-1) + fib(n-2);
     }
 }
